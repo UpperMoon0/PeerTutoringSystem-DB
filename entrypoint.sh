53fa23f602d679e732f63579e663b96153abbb48
NsTut <vonguyenminhnhatanphu@gmail.com>
2025-07-26 18:48:49 +0700

    Refactor entrypoint script to implement a dynamic wait for SQL Server startup instead of a fixed sleep duration

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,18 @@ set -e
 /opt/mssql/bin/sqlservr &
 
 # Wait for SQL Server to start
-sleep 15
+for i in {1..30};
+do
+    /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "Password123!" -Q "SELECT 1" &> /dev/null
+    if [ $? -eq 0 ]
+    then
+        echo "SQL Server is up"
+        break
+    else
+        echo "Waiting for SQL Server to start..."
+        sleep 1
+    fi
+done
 
 # Run the initialization and seeding scripts only if they haven't been run before
 if [ ! -f /var/opt/mssql/data/initialized.flag ]; then
